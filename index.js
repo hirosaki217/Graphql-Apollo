1e5c94d0f5579b6bb7babad93d413c11a6a3e9b1
Hiếu Trần <hirosaki217@gmail.com>
2022-08-24 16:03:25 +0700

    basic 2

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
 const { ApolloServer, gql } = require('apollo-server');
 
+const users = [
+    {
+        id: 1,
+        firstName: 'mukesh',
+        lastName: 'kumar',
+        email: 'mukesh@mukeshkumar.com',
+        password: '12345',
+    },
+    {
+        id: 2,
+        firstName: 'suresh',
+        lastName: 'sharma',
+        email: 'suresh@sureshkumar.com',
+        password: '12346',
+    },
+];
+
 const typeDefs = gql`
     type Query {
-        greet: String
+        getUsers: [User]!
+        getUser(id: ID!): User
+    }
+    type User {
+        id: ID!
+        firstName: String!
+        lastName: String!
+        email: String!
     }
 `;
 
 const resolvers = {
     Query: {
-        greet: () => 'Hello world',
+        getUsers: () => users,
+        getUser: (_, { id }) => {
+            return users.find((user) => user.id == id);
+        },
     },
 };
 
