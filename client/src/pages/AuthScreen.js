25e412216912645f3b9d06284c1f0565f465f840
Hiếu Trần <hirosaki217@gmail.com>
2022-08-28 15:05:56 +0700

    3.react ui

---
+++ client/src/pages/AuthScreen.js
@@ -0,0 +1,68 @@
+import React, { useRef, useState } from 'react';
+import { Box, Stack, Typography, Button, TextField, Card } from '@mui/material';
+
+const AuthScreen = () => {
+    const [showLogin, setShowLogin] = useState(true);
+    const [formData, setFormData] = useState({});
+    const authForm = useRef(null);
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(formData);
+    };
+    return (
+        <Box
+            ref={authForm}
+            component="form"
+            onSubmit={handleSubmit}
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            height="80vh"
+        >
+            <Card variant="outlined" sx={{ padding: '10px' }}>
+                <Stack direction="column" spacing={2} sx={{ width: '400px' }}>
+                    <Typography variant="h5">Please {showLogin ? 'Login' : 'Signup'}</Typography>
+                    {!showLogin && (
+                        <>
+                            <TextField name="firstName" label="First Name" variant="standard" onChange={handleChange} />
+                            <TextField name="lastName" label="Last Name" variant="standard" onChange={handleChange} />
+                        </>
+                    )}
+
+                    <TextField type="email" name="email" label="email" variant="standard" onChange={handleChange} />
+                    <TextField
+                        type="password"
+                        name="password"
+                        label="password"
+                        variant="standard"
+                        onChange={handleChange}
+                    />
+                    <Typography
+                        sx={{ cursor: 'pointer' }}
+                        textAlign="right"
+                        onClick={() => {
+                            setShowLogin((prv) => !prv);
+                            setFormData({});
+                            authForm.current.reset();
+                        }}
+                        variant="subtitle1"
+                    >
+                        {showLogin ? 'signup ?' : 'login ?'}
+                    </Typography>
+                    <Button variant="outlined" type="submit">
+                        {showLogin ? 'Login' : 'Signup'}
+                    </Button>
+                </Stack>
+            </Card>
+        </Box>
+    );
+};
+
+export default AuthScreen;
