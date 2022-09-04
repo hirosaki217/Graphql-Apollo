a9e0dfe72aface2710baf2336b21a6c04ae32f90
Hiếu Trần <hirosaki217@gmail.com>
2022-09-04 13:37:10 +0700

    setup apollo client

--- client/src/index.js
+++ client/src/index.js
@@ -5,11 +5,23 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const URL = 'http://localhost:5000/';
+
+const client = new ApolloClient({
+    uri: URL,
+    cache: new InMemoryCache(),
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
     <React.StrictMode>
         <BrowserRouter>
-            <App />
+            <ApolloProvider client={client}>
+                <App />
+            </ApolloProvider>
         </BrowserRouter>
     </React.StrictMode>,
 );
